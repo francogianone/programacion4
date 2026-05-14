ca4eef36e052a0d0bb14a348dc8bab9f0b3216a5
NahuelGodoy21 <ng879oooo@gmail.com>
2026-05-14 20:33:21 -0300

    CORRECCION DE ARCHIVOS DUPLICADOS!

--- src/routes/usuarios.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { registrarUsuario } = require('../controllers/usuarios.controller');
-
-router.post('/', registrarUsuario);
-
-module.exports = router;
