d72d4cae4abfcd935539b2e12a3fcb85d9ecb783
Robert <rober.villarrubiaa@gmail.com>
2026-05-23 19:07:05 -0300

    feat: implementar estado global y carrito de compras

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navigation/Navbar'
 import Footer from './components/Footer/Footer'
 import Products from './pages/Products'
 import ProductDetail from './pages/ProductDetail'
+import Cart from './pages/Cart'
 
 function App() {
   return (
@@ -15,12 +16,12 @@ function App() {
           { nombre: 'Productos', ruta: '/productos' },
           { nombre: 'Contacto', ruta: '/contacto' }
         ]}
-        cantidadCarrito={0}
       />
 
       <Routes>
         <Route path="/productos" element={<Products />} />
         <Route path="/productos/:id" element={<ProductDetail />} />
+        <Route path="/carrito" element={<Cart />} />
       </Routes>
 
       <Footer />
@@ -29,3 +30,4 @@ function App() {
 }
 
 export default App
+
